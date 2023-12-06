d52df5fc0cd99d71d6708223483345561e014606
Shashank Bandaru <shashanksr.bandaru@gmail.com>
2023-12-06 08:44:01 -0500

    day 5 startover

--- 05/day5.cpp
+++ 05/day5.cpp
@@ -6,63 +6,17 @@
 #include <vector>
 using namespace std;
 
-struct MapRange {
-    int64_t src_start;
-    int64_t dst_start;
-    int64_t dst_end;
+struct Range {
+    uint64_t start;
+    uint64_t end;
 
-    MapRange(int64_t src, int64_t dst, int64_t size) {
-        this->src_start = src;
-        this->dst_start = dst;
-        this->dst_end = dst + size - 1;
-    }
+    Range(uint64_t start, uint64_t size) : start(start), end(start + size - 1) {}
 };
-
-struct SeedRange {
-    int64_t start;
-    int64_t end;
-
-    SeedRange(int64_t start, int64_t size) : start(start), end(start + size - 1) {}
-};
-
-int64_t get_seed(const vector<vector<MapRange>> &maps, vector<map<int64_t, int64_t>> &lookups, int64_t loc) {
-    int64_t seed = loc;
-    for (int i = 6; i >= 0; i--) {
-        // Lookup if intermediary calculation has been done before.
-        // if (i != 0) {
-        //     auto it = lookups[i - 1].find(seed);
-        //     if (it != lookups[i - 1].end()) {
-        //         seed = it->second;
-        //         continue;
-        //     }
-        // }
-        
-
-        int64_t inter_val = seed;
-        for (MapRange range : maps[i]) {
-            if (seed < range.dst_start || seed > range.dst_end) continue;
-
-            seed = range.src_start + (seed - range.dst_start);
-            break;
-        }
-
-        // Store intermediary calculation.
-        // if (i != 0) {
-        //     lookups[i - 1].insert(pair<int64_t, int64_t>(inter_val, seed));
-        // }
-    }
-
-    return seed;
-}
 
 int main() {
-    vector<int64_t> seeds;
-    vector<SeedRange> seed_ranges;
-    vector<vector<MapRange>> maps(7);
-
-    // Lookup table stores intermediary calculations such as
-    // FERT - WATER, preventing unnecessary calculations.
-    vector<map<int64_t, int64_t>> lookups(6);
+    vector<Range> seeds;
+    vector<Range> seed_ranges;
+    vector<vector<pair<Range, Range>>> maps(7);
     string line, ignore;
 
     // Parsing
@@ -70,7 +24,6 @@ int main() {
     bool header = false;
     while (getline(cin, line)) {
         istringstream sin(line);
-        
 
         if (header) {
             header = false;
@@ -85,52 +38,35 @@ int main() {
 
         if (parsing_section == -1) {
             sin >> ignore; // Ignore 'seeds:'
-            int64_t seed;
+            uint64_t seed;
             while (sin >> seed) {
-                seeds.push_back(seed);
+                seeds.push_back(Range(seed, seed));
             }
 
             // Parse seed ranges for part 2.
             for (int i = 0; i < seeds.size(); i += 2) {
-                seed_ranges.push_back(SeedRange(seeds[i], seeds[i + 1]));
+                seed_ranges.push_back(Range(seeds[i].start, seeds[i + 1].start));
             }
+
+            // Condense ranges
+            sort(seed_ranges.begin(), seed_ranges.end(), [](const Range& a, const Range& b) {
+                return a.start < b.start;
+            });
+
+            for (int i = 0; i < seed_ranges.size(); i++) {
+                if (i + 1 < seed_ranges.size() || seed_ranges[i].end + 1 < seed_ranges[i + 1].start) continue;
+
+                seed_ranges[i].end = max(seed_ranges[i].end, seed_ranges[i + 1].end);
+                seed_ranges.erase(seed_ranges.begin() + i + 1);
+                i--;
+            }
+
             continue;
         }
 
-        // Store maps into maps.
-        int64_t src, dst, size;
+        // Store maps.
+        uint64_t src, dst, size;
         sin >> dst >> src >> size;
-        maps[parsing_section].push_back(MapRange(src, dst, size));
+        maps[parsing_section].push_back(pair(Range(src, size), Range(dst, size)));
     }
-
-    // Part 1 Logic
-    // Find least valued location out of all seeds.
-    int64_t min_1 = 0;
-    while (true) {
-        int64_t seed = get_seed(maps, lookups, min_1);
-        cout << "Loc: " << min_1 << " Seed: " << seed << endl << flush;
-        if (find(seeds.begin(), seeds.end(), seed) != seeds.end()) {
-            break;
-        }
-
-        min_1++;
-    }
-
-    // Part 2 Logic
-    // Find least valued location out of all seed ranges.
-    int64_t min_2 = 0;
-    while (true) {
-        int64_t seed = get_seed(maps, lookups, min_2);
-        cout << "Loc: " << min_2 << " Seed: " << seed << endl;
-        if (any_of(seed_ranges.begin(), seed_ranges.end(), [seed](const SeedRange &sr){
-            return seed >= sr.start && seed <= sr.end;
-        })) break;
-
-        min_2++;
-    }
-
-    cout << "Part 1" << endl;
-    cout << "Lowest location number is: " << min_1 << endl;
-    cout << "Part 2" << endl;
-    cout << "Lowest location number is: " << min_2 << endl;
 }

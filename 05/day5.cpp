47861d8dce23c4b82a0892b77de2b9c7fed16713
Shashank Bandaru <shashanksr.bandaru@gmail.com>
2023-12-06 12:01:39 -0500

    better solution but doesnt work fully yet

--- 05/day5.cpp
+++ 05/day5.cpp
@@ -13,6 +13,51 @@ struct Range {
     Range(uint64_t start, uint64_t size) : start(start), end(start + size - 1) {}
 };
 
+void transform_ranges(vector<Range> &seeds, const vector<pair<Range, Range>> &map) {
+    vector<Range> transformed;
+    for (auto seed : seeds) {
+        for (auto range : map) {
+            Range new_range = seed;
+            if (seed.start > range.first.end || seed.end < range.first.start) continue;
+
+            if (seed.start >= range.first.start) {
+                new_range.start = range.second.start + (seed.start - range.first.start);
+            }
+            else {
+                new_range.start = range.second.start;
+                transformed.push_back(Range(seed.start, range.first.start - seed.start));
+            }
+
+
+            if (seed.end <= range.first.end) {
+                new_range.end = range.second.start + (seed.end - range.first.start);
+            }
+            else {
+                new_range.end = range.second.end;
+                transformed.push_back(Range(range.first.end + 1, seed.end - range.first.end));
+            }
+
+            transformed.push_back(new_range);
+        }
+    }
+
+    seeds = transformed;
+}
+
+void condense_ranges(vector<Range> &ranges) {
+    sort(ranges.begin(), ranges.end(), [](const Range& a, const Range& b) {
+        return a.start < b.start;
+    });
+
+    for (int i = 0; i < ranges.size(); i++) {
+        if (i + 1 < ranges.size() && ranges[i].end + 1 >= ranges[i + 1].start) {
+            ranges[i].end = std::max(ranges[i].end, ranges[i + 1].end);
+            ranges.erase(ranges.begin() + i + 1);
+            i--;
+        }
+    }
+}
+
 int main() {
     vector<Range> seeds;
     vector<Range> seed_ranges;
@@ -49,17 +94,7 @@ int main() {
             }
 
             // Condense ranges
-            sort(seed_ranges.begin(), seed_ranges.end(), [](const Range& a, const Range& b) {
-                return a.start < b.start;
-            });
-
-            for (int i = 0; i < seed_ranges.size(); i++) {
-                if (i + 1 < seed_ranges.size() || seed_ranges[i].end + 1 < seed_ranges[i + 1].start) continue;
-
-                seed_ranges[i].end = max(seed_ranges[i].end, seed_ranges[i + 1].end);
-                seed_ranges.erase(seed_ranges.begin() + i + 1);
-                i--;
-            }
+            condense_ranges(seed_ranges);
 
             continue;
         }
@@ -69,4 +104,26 @@ int main() {
         sin >> dst >> src >> size;
         maps[parsing_section].push_back(pair(Range(src, size), Range(dst, size)));
     }
+
+    // Part 1
+    for (auto map : maps) {
+        transform_ranges(seeds, map);
+    }
+    condense_ranges(seeds);
+
+    for (auto seed : seeds) {
+        cout << "[" << seed.start << ", " << seed.end << "], ";
+    }
+    cout << endl;
+    
+    // Part 2
+    for (auto map : maps) {
+        transform_ranges(seed_ranges, map);
+    }
+    condense_ranges(seed_ranges);
+
+    for (auto seed : seed_ranges) {
+        cout << "[" << seed.start << ", " << seed.end << "], ";
+    }
+    cout << endl;
 }
